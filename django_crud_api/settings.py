037afdfc26c3071e8428071ef02ad35f100eb90a
Nilton taco <nilton2005marzo@gmail.com>
2024-11-17 15:33:07 -0500

    delete a bug

--- django_crud_api/settings.py
+++ django_crud_api/settings.py
@@ -137,12 +137,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'static')
-    ] 
+
 # This production code might break development mode, so we check whether we're in DEBUG mode
 if not DEBUG:
-    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+    
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
