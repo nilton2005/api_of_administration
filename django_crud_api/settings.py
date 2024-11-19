8259175a4dc65295b02fdd51fae09494aa3b5c57
Nilton taco <nilton2005marzo@gmail.com>
2024-11-19 18:03:47 -0500

    allow all allowed host

--- django_crud_api/settings.py
+++ django_crud_api/settings.py
@@ -25,9 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 DEBUG = 'RENDER' not in os.environ
 ALLOWED_HOSTS = [
-    'localhost',
-    '127.0.0.1',
-    '192.168.43.73',
+   '*'
+
 ]
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
