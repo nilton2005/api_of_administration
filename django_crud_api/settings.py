0bc1087be3fcc604391f53a6e142f348621fe0c6
Nilton taco <nilton2005marzo@gmail.com>
2024-11-21 18:43:47 -0500

    fix settign for cors origin 4

--- django_crud_api/settings.py
+++ django_crud_api/settings.py
@@ -156,7 +156,15 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 # CORS settings 
-CORS_ALLOWED_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173',
+    'http://127.0.0.1',
+    'http://192.168.43.73',
+]
+
+# Permitir acceso desde cualquier origen (no recomendado para producción)
+CORS_ALLOW_ALL_ORIGINS = True
 
 # settings.py
 REST_FRAMEWORK = {
